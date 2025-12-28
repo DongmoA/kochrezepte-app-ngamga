61e08554b51b20c44a988ab5ac35a24beb9a2671
Ken Ulrich Nya Ngamga <146624806+kenulrichnyangamga@users.noreply.github.com>
2025-12-28 20:28:10 +0100

    feat: Ausloggen-Button in HomePage hinzugefügt

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:kochrezepte_app/supabase/auth_service.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              try {
+                await _authService.signOut();
+                print('Déconnexion réussie !');
+              } catch (e) {
+                print('Erreur : $e');
+              }
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to the Home Page!'),
