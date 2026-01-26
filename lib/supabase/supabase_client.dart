dc03cf86feae42493c86e43d18cf91964d28c038
@mdmm94 <manuela.djomkam.moukam@mni.thm.de>
2026-01-26 22:52:53 +0100

    .env hinzugefpgt

--- lib/supabase/supabase_client.dart
+++ lib/supabase/supabase_client.dart
@@ -7,7 +7,7 @@ class SupabaseClientManager {
 
   static Future<void> initialize() async {
 
-    await dotenv.load(fileName: ".env");
+    await dotenv.load(fileName: ".env"); 
 
     final supabaseUrl = dotenv.env['SUPABASE_URL']?? '';
     final supabaseKey = dotenv.env['SUPABASE_KEY'] ?? ''; 
